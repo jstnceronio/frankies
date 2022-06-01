e99fe54bb700132d53bc138061be1b1fa240b43d
Justin Ceronio <justin.ceronio@outlook.com>
2022-06-01 20:52:49 +0200

    Add xs breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
     fontFamily: {
       LuxuriousRoman: ["'Luxurious Roman', cursive"],
